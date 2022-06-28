a71d2620e3e9e294a35dfc090b41324503f55f68
Hazumiran <diazazhari32@gmail.com>
2022-06-28 22:46:45 +0700

    myscheduler

---
+++ README.md
@@ -0,0 +1,3 @@
+#React Js with Firebase
+My scheduler
+![task](https://user-images.githubusercontent.com/65151166/176220418-7b6617ee-0f73-4811-a975-3aa8f1709d25.png)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+
+function App() {
+  
+  return (
+    <div className='app'>
+      <h1 >Mau running apa bro ?? wkwk</h1>
+    </div>
+  );
+}
+
+export default App;
